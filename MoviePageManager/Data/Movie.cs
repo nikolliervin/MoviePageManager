9e603fb7d5c4c2f329b5c3ef35914bfeeec736a9
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-02-25 21:46:07 +0100

    Database Manager

---
+++ MoviePageManager/Data/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviePageManager.Data
+{
+
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string MovieName { get; set; }
+		public int Year { get; set; }
+	}
+}
--- MoviePageManager/Data/MovieDBContext.cs
+++ MoviePageManager/Data/MovieDBContext.cs
@@ -24,11 +24,5 @@ namespace MoviePageManager.Data
 		}
 	}
 
-	public class Movie
-	{
-		public int Id { get; set; }
-		public string MovieName { get; set; }
-		public int Year { get; set; }
-	}
 }
 
