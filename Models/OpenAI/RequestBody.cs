7678340817455dd6b1d3694c837ce1153c5df2ca
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-02-25 19:18:00 +0100

    Open AI Service

---
+++ Models/OpenAI/RequestBody.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.OpenAI
+{
+	internal class RequestBody
+	{
+
+		public string model { get; set; }
+		public string prompt { get; set; }
+		public int max_tokens { get; set; }
+		public int temperature { get; set; }
+		public int top_p { get; set; }
+		public int n { get; set; }
+		public bool stream { get; set; }
+		public object logprobs { get; set; }
+		public string stop { get; set; }
+
+	}
+}
