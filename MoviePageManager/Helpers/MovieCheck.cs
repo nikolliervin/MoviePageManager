21e2c687100bfc1b10c1993d166191ee8bedc049
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-02-25 21:53:40 +0100

    Unique Movie Check

---
+++ MoviePageManager/Helpers/MovieCheck.cs
@@ -0,0 +1,34 @@
+﻿using MoviePageManager.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviePageManager.Helpers
+{
+	public class MovieCheck
+	{	
+		private readonly DbManager _dbManager;
+		public MovieCheck(DbManager dbManager)
+		{
+			_dbManager = dbManager;
+		}
+		public bool existsMovie(string movieName, string year)
+		{
+			List<Movie> movies = _dbManager.getMovies();
+
+			foreach(var movie in movies)
+			{
+				if(movie.MovieName == movieName)
+				{
+					return true;
+				}
+				
+			}
+
+			return false;
+
+		}
+	}
+}
