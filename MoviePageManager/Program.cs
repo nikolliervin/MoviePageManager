8146df23e48595e7cbe374b798b2b803d0b275b1
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-02-25 19:49:29 +0100

    Request

--- MoviePageManager/Program.cs
+++ MoviePageManager/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using MoviePageManager.OpenAI;
+using System;
+using Models.OpenAI;
+using System.Configuration;
 
 namespace MoviePageManager
 {
-	internal class Program
+	public class Program
 	{
+	
 		static void Main(string[] args)
 		{
-			
+			var openAIKey = Environment.GetEnvironmentVariable("openAIKey");
+
+			var _openAIService = new OpenAIService(openAIKey);
+
+
+
+			var request = new RequestBody()
+			{
+				model = ConfigurationManager.AppSettings[openAIKey]
+			};
+
+
 		}
 	}
 }
