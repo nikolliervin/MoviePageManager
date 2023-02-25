039b11c6868b589df75669e4069633b6bfce77a1
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-02-25 22:19:34 +0100

    Adding movies to Db

--- MoviePageManager/Program.cs
+++ MoviePageManager/Program.cs
@@ -4,29 +4,45 @@ using Models.OpenAI;
 using System.Configuration;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using MoviePageManager.Helpers;
+using System.Net.Http.Headers;
+using Microsoft.EntityFrameworkCore;
+using MoviePageManager.Data;
 
 namespace MoviePageManager
 {
 	public class Program
 	{
-	
+		
+
 		static async Task Main(string[] args)
 		{
 			var openAIKey = Environment.GetEnvironmentVariable("openAIKey");
 
 			var _openAIService = new OpenAIService(openAIKey);
+			var helpers = new Helpers.Helpers();
+			using var dbContext = new MovieDBContext();
+			var dbManager = new DbManager(dbContext);
 
-			var prompt = "Give me a movie name with a ration of over 8.5";
+
+			var prompt = helpers.firstPrompt();
 
 
-			var response = await _openAIService.SendRequestAsync(prompt);
+			var firstResponse = await _openAIService.SendRequestAsync(prompt);
 
-			var choiceObj = JsonConvert.DeserializeObject<ResponseBody>(response).Choices[0];
+			var choiceObj = JsonConvert.DeserializeObject<ResponseBody>(firstResponse).Choices[0].Text;
 
-			var nextPromp = "Write a summary about the movie:" + choiceObj.Text + " without spoliers";
+			var movie = helpers.getMovieObj(choiceObj);
 
-			var secondResp = await _openAIService.SendRequestAsync(nextPromp);
+			
+			dbManager.addMovie(movie);
+
+			var nextPrompt = helpers.secondPrompt(movie.MovieName, movie.Year.ToString());
+
+			var secondResp = await _openAIService.SendRequestAsync(nextPrompt);
 
 		}
+
+	
 	}
 }
